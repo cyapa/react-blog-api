e1e78ebb48ed7c523e91b4a080950049cced9707
Chathura Yapa <chathura.yapa@lionbridge.com>
2021-12-01 19:41:12 +0200

    Changed connections tring

--- api/session.py
+++ api/session.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 #TODO: Get this via a KMS
-POSTGRES_URL = "postgres://aitkyjrlwvciaq:ca20df7506e91601e3233796eecd9e61335b76095428aaeae2c54d271e026dbb@ec2-54-228-139-34.eu-west-1.compute.amazonaws.com:5432/dc3790mkg38clg"
+POSTGRES_URL = "postgresql://aitkyjrlwvciaq:ca20df7506e91601e3233796eecd9e61335b76095428aaeae2c54d271e026dbb@ec2-54-228-139-34.eu-west-1.compute.amazonaws.com:5432/dc3790mkg38clg"
 
 
 engine = create_async_engine(POSTGRES_URL.replace("postgresql://", "postgresql+asyncpg://"), pool_size=100, future=True)
