85622a7cc0b4f96a5c668e494bf12a6b4b4807b8
Chathura Yapa <chathura.yapa@lionbridge.com>
2021-12-04 14:29:58 +0200

    Comment model class added

--- api/db_provider.py
+++ api/db_provider.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, DateTime, Integer, String, text,Boolean
+from sqlalchemy import Column, DateTime, Integer, String, text,Boolean,ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 from api.session import AsyncSessionLocal
@@ -19,6 +19,20 @@ class Blog(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
     content = Column(String(1000), nullable=False)
+    is_deleted = Column(Boolean,nullable=False, default=False)
+    ctime = Column(
+        DateTime(timezone=True), nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
+    mtime = Column(
+        DateTime(timezone=True), nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
+
+class Comment(Base):
+    __tablename__ = "comment"
+
+    id = Column(Integer, primary_key=True)
+    blog_id = Column(ForeignKey(Blog.id), nullable=False)
+    comment = Column(String(1000), nullable=False)
     is_deleted = Column(Boolean,nullable=False, default=False)
     ctime = Column(
         DateTime(timezone=True), nullable=False, server_default=text("CURRENT_TIMESTAMP")
